cea380104ce97d3a22f22e0326240239ac7e85dd
Arthur Allshire <arthur.allshire@gmail.com>
2015-11-08 10:35:27 +1100

    Add basic 'session' functionality to the state machine

--- state-test.py
+++ state-test.py
@@ -5,6 +5,7 @@ class Person(object):
         self.name = name
         self.qr_text = qr_text
         self._state = False #True = inside (clocking up time), False = outside (not clocking up time)
+        #   Example structure for logs: [[["IN", 1446938256], ["OUT", 1446939200]], [["IN", 1447941200]]]
         self._logs = []
 
     def match_qr(self, qr_text):
@@ -16,10 +17,15 @@ class Person(object):
 
     def log(self):
         """ Called if the qr string scanned matches this person's one. If it does, then the log() method is called to log it"""
-        if self._state == False:
-            self._logs.append(["IN", calendar.timegm(time.gmtime())])
+        if self._state == True:
+            if len(self._logs[-1]) == 1 and self._logs[-1][0][0] == "IN":
+                    # there is an open session at the end of the logs list that needs to be closed
+                    self._logs[-1].append(["OUT", calendar.timegm(time.gmtime())])
+                # Here I need to figure out what to do if there is there is a session that closes with no opening,
+                # as well as what if there is a session that has "OUT" at the start
         else:
-            self._logs.append(["OUT", calendar.timegm(time.gmtime())])
+            # this person is currently logged out, therefore we want to add a new session on the end of the list
+            self._logs.append([["IN", calendar.timegm(time.gmtime())]])
         self._state = not self._state
 
 arthur = Person("Arthur Allshire", "shots fired")
@@ -29,6 +35,13 @@ print arthur._logs
 
 print arthur.match_qr("shots fired")
 print arthur._logs
+
+print arthur.match_qr("shots fired")
+print arthur._logs
+
+print arthur.match_qr("shots fired")
+print arthur._logs
+
 
 print arthur.match_qr("shots fired")
 print arthur._logs
