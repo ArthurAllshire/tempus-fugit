ad6a0f74e9412ac10be171f2e584745f8655ce35
Arthur Allshire <arthur.allshire@gmail.com>
2015-11-08 11:10:09 +1100

    Add support for different locations

--- state-test.py
+++ state-test.py
@@ -8,40 +8,45 @@ class Person(object):
         #   Example structure for logs: [[["IN", 1446938256], ["OUT", 1446939200]], [["IN", 1447941200]]]
         self._logs = []
 
-    def match_qr(self, qr_text):
+    def match_qr(self, qr_text, location):
         """ Method that you can call on this object to see if it's qr code string matches a specific one. If it does, returns True"""
         if self.qr_text == qr_text:
-            self.log()
+            self.log(location)
             return [True, self.name]
         return [False, self.name]
 
-    def log(self):
+    def log(self, location):
         """ Called if the qr string scanned matches this person's one. If it does, then the log() method is called to log it"""
         if self._state == True:
-            if len(self._logs[-1]) == 1 and self._logs[-1][0][0] == "IN":
+            if len(self._logs[-1]) == 2 and self._logs[-1][1][0] == "IN":
                     # there is an open session at the end of the logs list that needs to be closed
                     self._logs[-1].append(["OUT", calendar.timegm(time.gmtime())])
+                    if not self._logs[-1][0] == location:
+                        self._logs.append([location, ["IN", calendar.timegm(time.gmtime())]])
+                    else:
+                        self._state = not self._state
                 # Here I need to figure out what to do if there is there is a session that closes with no opening,
                 # as well as what if there is a session that has "OUT" at the start
         else:
             # this person is currently logged out, therefore we want to add a new session on the end of the list
-            self._logs.append([["IN", calendar.timegm(time.gmtime())]])
-        self._state = not self._state
+            self._logs.append([location, ["IN", calendar.timegm(time.gmtime())]])
+            self._state = not self._state
 
 arthur = Person("Arthur Allshire", "shots fired")
 
-print arthur.match_qr("example")
+print arthur.match_qr("example", "Mechatronics Lab")
 print arthur._logs
 
-print arthur.match_qr("shots fired")
+print arthur.match_qr("shots fired", "Mechatronics Lab")
 print arthur._logs
 
-print arthur.match_qr("shots fired")
+print "Workshop: "
+print arthur.match_qr("shots fired", "Workshop")
 print arthur._logs
 
-print arthur.match_qr("shots fired")
+print arthur.match_qr("shots fired", "Mechatronics Lab")
 print arthur._logs
 
 
-print arthur.match_qr("shots fired")
+print arthur.match_qr("shots fired", "Mechatronics Lab")
 print arthur._logs
